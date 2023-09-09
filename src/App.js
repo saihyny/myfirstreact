a4141b7526a347526eebb61c8949b2a1ac0d544a
saihyny <satyasaikiranrocks@gmail.com>
2023-09-09 22:12:45 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expensitem from './Components/Expens';
+
 import './App.css';
 
 function App() {
--- src/Components/Expens.js
+++ src/Components/Expens.js
@@ -1,8 +1,22 @@
-
-function Expensitem()
-{
-    return(
-     <><h4> food - 10</h4><h4>petrol - 100</h4><h4> movies - 200</h4></>
-    )
+import './Expens.css'
+function Expensitem() {
+ const expenseDate = new Date(2023,10,10)
+ const expenseItem = 'car insurence'
+ const expensePrice = 200
+ const LocationOfExpenditure = 'Dmart'
+ 
+ 
+ 
+    return (
+    <div className="expense-item">
+      <div>{expenseDate.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{expenseItem}</h2>
+        <div>{LocationOfExpenditure}</div>
+        <div className="expense-item__price">${expensePrice}</div>
+        
+      </div>
+    </div>
+  );
 }
 export default Expensitem;
