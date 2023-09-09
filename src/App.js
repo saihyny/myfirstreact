831ff35bb3f8a4270d3cb1669564bea44a782265
saihyny <satyasaikiranrocks@gmail.com>
2023-09-09 12:12:29 +0530

    expense items

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+import Expensitem from './Components/Expens';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <h2>Expense items</h2> 
+     <Expensitem></Expensitem>
+     
     </div>
   );
 }
---
+++ src/Components/Expens.js
@@ -0,0 +1,8 @@
+
+function Expensitem()
+{
+    return(
+     <><h4> food - 10</h4><h4>petrol - 100</h4><h4> movies - 200</h4></>
+    )
+}
+export default Expensitem;
