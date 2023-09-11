9fc102f9aa716a0e82eacf3741d1a5d179174864
saihyny <satyasaikiranrocks@gmail.com>
2023-09-11 12:22:40 +0530

    usingPropsConsept

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import Expensitem from './Components/Expens';
+import Expensitem from "./Components/Expens";
 
-import './App.css';
+import "./App.css";
 
 function App() {
+  const expense = [
+    {
+      id: "e1",
+      title: "Toilet paper",
+      amount: 88.58,
+      date: new Date(2020, 7, 12),
+      LocationOfExpenditure: "Dmart usa",
+    },
+  ];
   return (
     <div>
-     <h2>Expense items</h2> 
-     <Expensitem></Expensitem>
-     
+      <h2>Expense items</h2>
+      <Expensitem
+        title={expense[0].title}
+        amount={expense[0].amount}
+        date={expense[0].date}
+        location={expense[0].LocationOfExpenditure}
+      ></Expensitem>
     </div>
   );
 }
--- src/Components/Expens.js
+++ src/Components/Expens.js
@@ -1,20 +1,12 @@
-import './Expens.css'
-function Expensitem() {
- const expenseDate = new Date(2023,10,10)
- const expenseItem = 'car insurence'
- const expensePrice = 200
- const LocationOfExpenditure = 'Dmart'
- 
- 
- 
-    return (
+import "./Expens.css";
+function Expensitem(props) {
+  return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseItem}</h2>
-        <div>{LocationOfExpenditure}</div>
-        <div className="expense-item__price">${expensePrice}</div>
-        
+        <h2>{props.title}</h2>
+        <h2>{props.location}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
