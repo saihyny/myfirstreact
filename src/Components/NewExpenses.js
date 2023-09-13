82ee0be9998741881d51c603014f3c4214cbb62d
saihyny <satyasaikiranrocks@gmail.com>
2023-09-13 14:56:48 +0530

    loggingSubjectUsersText

---
+++ src/Components/NewExpenses.js
@@ -0,0 +1,11 @@
+import ExpenseForm from './ExpenseForm'
+import './ExpensesForm.css'
+import React from 'react'
+const NewExpenses =()=>{
+return (
+<div className='new-expense' >
+<ExpenseForm></ExpenseForm>
+</div>
+)
+}
+export default NewExpenses
